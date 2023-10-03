4f8c7f3a520889bfe5b5511604721304b906efa1
이건창 <geonc123@estsoft.com>
2023-10-03 11:34:50 +0900

    refact : 글 수정

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,7 @@
 const projectsData = [
   {
     title: '팀업 서비스 종료 과정',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    description: `팀업 서비스의 종료하게 되면서 사용자들의 데이터를 백업하는 과정을 거치고 있습니다.`,
     imgSrc: '/static/images/project-teamup.png',
     href: 'https://tmup.com/notice?device=pc',
   },
