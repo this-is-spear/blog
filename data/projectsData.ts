fd33028e16f2a4367ff5aa47dda227db48b6c3b8
이건창 <92219795+this-is-spear@users.noreply.github.com>
2023-11-12 11:45:48 +0900

    dcos : 글 작성

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,7 +3,7 @@ const projectsData = [
     title: '팀업 서비스 종료 과정',
     description: `팀업 서비스의 종료하게 되면서 사용자들의 데이터를 백업하는 과정을 거치고 있습니다.`,
     imgSrc: '/static/images/project-teamup.png',
-    href: 'https://tmup.com/notice?device=pc',
+    href: 'https://tis-blog.vercel.app/blog/project/teamup-backup',
   },
 ]
 
