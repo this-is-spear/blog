a9509f1147635ac4786ea7383a754844fd98cabb
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-04-15 23:28:11 +0900

    docs : 글 작성

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,12 +5,6 @@ const projectsData = [
     imgSrc: '/static/images/project-teamup.png',
     href: 'https://tis-blog.vercel.app/blog/project/teamup-backup',
   },
-  {
-    title: '이미지 리사이징 서비스',
-    description: `사용자에게 전달될 정적 이미지를 어떻게 하면 잘 관리 할 수 있을지 프론트엔드 입장에서 고민 했습니다.`,
-    imgSrc: '/static/images/project/fly-away-resized-image/3.png',
-    href: 'https://tis-blog.vercel.app/blog/project/fly-away-resized-image',
-  },
 ]
 
 export default projectsData
