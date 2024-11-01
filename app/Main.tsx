cad9597a837f2b14c2ea5d15cb751ec26c8f8d4c
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-11-01 16:50:48 +0900

    dcos : vercel 추가

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { Analytics } from '@vercel/analytics/react';
 
 const MAX_DISPLAY = 5
 
@@ -86,6 +87,7 @@ export default function Home({ posts }) {
           <NewsletterForm />
         </div>
       )}
+    <Analytics />
     </>
   )
 }
